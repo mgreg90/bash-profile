bc9c497a2e29c5051b275218fc5b23cea2504977
Mike Gregory <mgregory8219@gmail.com>
2017-06-23 00:38:57 -0400

    Fixed issue with git tracking

--- secret_functions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "\tloading bash aliases..."
-
-  # some functions
-
-echo "\t\tDone"
--- secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "loading bash secrets..."
-
-export SOME_TOKEN="ImADopeAssToken"
-export SOME_SECRET="ImAnEvenDoperSecret"
-
-echo "Done"
